6950d8dcdd89db5e3f9cd909c1bd942644d48c0a
NIK Nerkar <iamp1.nik@outlook.com>
2023-07-08 15:49:08 +0530

    Progress-Bar

---
+++ 02-Progress-Steps/script.js
@@ -0,0 +1,45 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("#prev");
+
+const next = document.getElementById("#next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+    // next.classList.add('')
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+const update = function () {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  progress.style.width = (currentActive - 1) * 33.33 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+};
