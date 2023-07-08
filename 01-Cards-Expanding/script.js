bacc67242d29a3ea6bc1265138f2eee0a2b64c4c
NIK Nerkar <iamp1.nik@outlook.com>
2023-07-08 15:04:04 +0530

    Animated-Card-Click-Reveal

--- 01-Cards-Expanding/script.js
+++ 01-Cards-Expanding/script.js
@@ -1,1 +1,14 @@
-alert("Hello there!");
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClasses = function () {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
